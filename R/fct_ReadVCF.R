47aacb453797f09ff9dde1c9b613101e4753a79f
cwbcm <chen.wang@bcm.edu>
2023-06-02 12:42:00 -0500

    Update ReadVCF

--- R/fct_ReadVCF.R
+++ R/fct_ReadVCF.R
@@ -3,10 +3,11 @@
 #' @description Read the entries from the vcf files, headers are removed.
 #' @author Chen Wang
 #' @param path Path to the vcf file
+#' @param header_max Max number of rows to guess VCF header lines.
 #' @return A tibble containing all the entries in the VCF file.
 #' @export
-ReadVCF <- function(path) {
-  vcf.temp <- readr::read_lines(path)
+ReadVCF <- function(path, header_max = 100) {
+  vcf.temp <- readr::read_lines(path, n_max = header_max)
   skip.row <- grep(pattern = "#CHROM", vcf.temp) - 1 # remove info rows from the vcf header
   vcf <- readr::read_tsv(file = path, skip = skip.row,
                          col_types = readr::cols_only(`#CHROM` = "c",
